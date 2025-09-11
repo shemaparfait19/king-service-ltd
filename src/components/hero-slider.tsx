e200723838e2efd72e267f7ec4af502a32c15ee8
century soap <centurysoap1@gmail.com>
2025-09-11 10:53:45 +0000

    alright but still now images are not displaying at all on lading page

--- src/components/hero-slider.tsx
+++ src/components/hero-slider.tsx
@@ -30,9 +30,9 @@ export default function HeroSlider() {
         onMouseEnter={plugin.current.stop}
         onMouseLeave={plugin.current.reset}
       >
-        <CarouselContent className="h-full">
+        <CarouselContent className="h-full m-0">
           {heroImages.map((image, index) => (
-            <CarouselItem key={image.id} className="h-full">
+            <CarouselItem key={image.id} className="h-full p-0">
               <div className="relative w-full h-full">
                 <Image
                   src={image.imageUrl}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
-        code: ['monospace'],
+        body: ['var(--font-inter)'],
+        headline: ['var(--font-space-grotesk)'],
       },
       colors: {
         border: "hsl(var(--border))",
